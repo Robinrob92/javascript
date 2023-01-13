d40ecdfdb9c5343327a45a3d68c8c2feb32b888d
Robin Chriqui <robin.chriqui@gmail.com>
2023-01-13 09:34:46 +0200

    created facebook2

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var database=
     {
         username:"andrei",
         password:"secret"
+    },
+    {
+        username:'sally',
+        password:'123'
+    },
+    {
+        username:'ingrid',
+        password:'777'
     }
 ];
 
@@ -22,7 +30,18 @@ var userNamePrompt=prompt("What is your username?");
 var passwordPrompt=prompt("What is your password?");
 
 function signIn(username, password){
-    if (username===database[0].username &&
+    for (var i=0; i<database.length; i++){
+        if (database[i].username===username &&
+            database[i].password===password
+        ) {
+            console.log(news_feed);
+        } else {
+            alert{
+                "Sorry, wrong username and password"
+            }
+        }
+    
+    /* if (username===database[0].username &&
         password===database[0].password
         )
         {
@@ -30,7 +49,7 @@ function signIn(username, password){
         }
     else {
             alert("sorry wrong username or password")
-        }
+        } */
 }
 
 signIn(userNamePrompt, passwordPrompt);
